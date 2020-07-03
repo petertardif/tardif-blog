5494c4e0326710f5456a1777d001ccda9dfc9bf3
Peter Tardif <peter.tardif@gmail.com>
2020-07-03 08:02:13 -0600

    updating form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -12,24 +12,25 @@ function ContactForm(props) {
     resetEmail();
     resetMessage();
   }
+
   return (
     <form method="post" onSubmit={handleSubmit} netlify-honeypot="bot-field" data-netlify="true" name="contact">
-      <input type="hidden" name="bot-field" />
-      <input type="hidden" name="form-name" value="contact" />
-      <label>
+      <input type="hidden" name="bot-field" aria-label="Netlify hidden input to handle submit" />
+      <input type="hidden" name="form-name" value="contact" aria-label="Netlify hidden input to handle submit" />
+      <label htmlFor="name">
         Name
-        <input type="text" name="name" id="name"{...bindName} />
+        <input type="text" name="name" id="name" {...bindName} aria-label="Enter Name Field" />
       </label>
-      <label>
+      <label htmlFor="email">
         Email
-        <input type="email" name="email" id="email"{...bindEmail} />
+        <input type="email" name="email" id="email" {...bindEmail} aria-label="Enter Email Field" />
       </label>
-      <label>
+      <label htmlFor="message">
         Message
         {/* <input type="text" {...bindMessage} /> */}
-        <textarea name="message" id="message" rows="5" {...bindMessage} />
+        <textarea name="message" id="message" rows="5" {...bindMessage} aria-label="Enter Message Field" />
       </label>
-      <input type="submit" value="Send Message" />
+      <input type="submit" value="Send Message" aria-label="Submit button" />
     </form>
   )
 }
