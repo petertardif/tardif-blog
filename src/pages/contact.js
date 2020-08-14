9b870474dd13c808444191cf73fe356c21c240b4
Peter Tardif <peter.tardif@gmail.com>
2020-08-14 00:09:13 -0600

    fixing 304 status error

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,5 @@
 import React from "react"
-// import Layout from "../components/layout";
-import Header from "../components/header";
-import Footer from "../components/footer";
+import Layout from "../components/layout";
 import ContactForm from '../components/ContactForm';
 import styled from "styled-components";
 
@@ -21,13 +19,13 @@ const ContactFormContainer = styled.div`
 
 export default () => {
   return (
-    <ContactFormContainer>
-      <Header />
-      <div className="formContainer">
-        <h1>Contact</h1>
-        <ContactForm />
-      </div>
-      <Footer />
-    </ContactFormContainer>
+    <Layout>
+      <ContactFormContainer>
+        <div className="formContainer">
+          <h1>Contact</h1>
+          <ContactForm />
+        </div>
+      </ContactFormContainer>
+    </Layout>
   )
 }
